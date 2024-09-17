941387cf44cc4c06d8b7ff72f65b2bef8e1780db
hritikcore <hritiksethi@Hritiks-MacBook-Pro.local>
2024-09-17 14:27:58 +0530

    check url

--- api/redirect.js
+++ api/redirect.js
@@ -1,13 +1,20 @@
+import { createProxyMiddleware } from "http-proxy-middleware";
+
 export default function handler(req, res) {
   const userAgent = req.headers["user-agent"] || "";
 
   const isMobile = /mobile|android|iphone|ipad/i.test(userAgent);
 
-  // If mobile, serve content from the mobile version
-  if (isMobile) {
-    res.redirect(308, "https://maahermobile.vercel.app");
-  } else {
-    // If desktop, serve content from the desktop version
-    res.redirect(308, "https://maaher.vercel.app");
-  }
+  const targetUrl = isMobile
+    ? "https://maahermobile.vercel.app"
+    : "https://maaher.vercel.app";
+
+  // Proxy the request to the appropriate target based on device type
+  return createProxyMiddleware({
+    target: targetUrl,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/": "/",
+    },
+  })(req, res);
 }
